8268349ec55018d705b232d6b764b27a384786c5
Lucas <lucasalves_07@outlook.com>
2026-05-15 23:12:29 -0300

    Feito a logica de pegar os dados do card para preencher o formulario Pequenos detalhes visuais feito no formulario Nome do arquivo js alterado de app.js----script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// faz uma lista com todos os itens com classe = btn
+const botoes = document.querySelectorAll('.btn');
+
+// percorrendo a lista de botões
+botoes.forEach(botao => {
+
+  // adicionando o evento de click
+  botao.addEventListener('click', (event) => {
+
+    // pega o card que foi clicado
+    const card = event.target.closest('.card');
+
+    // criando variaveis com os textos 
+    const titulo = card.querySelector('h3').textContent;
+    const tempo = card.querySelector('span').textContent;
+    const valor = card.querySelector('strong').textContent;
+
+    // salva os dados no navegador
+    const dados = { titulo, tempo, valor };
+    localStorage.setItem('agendamento', JSON.stringify(dados));
+
+    // vai para outra página
+    window.location.href = 'agendamento.html';
+  });
+});
+
+// pegar os dados na pagina de agendamento
+const tituloAgendamento = document.getElementById('tituloAgendamento');
+
+const tempoValor = document.getElementById('tempoValor');
+
+if (tituloAgendamento) {
+  const dados = JSON.parse(localStorage.getItem('agendamento'));
+  if (dados) {
+    tituloAgendamento.textContent = `${dados.titulo}`;
+    tempoValor.textContent = `${dados.tempo} • ${dados.valor}`
+  }
+}
